2758898018942da48ab4dea743e36ba27aecf948
Gustavo dos Santos Leão <102435052+GustavoSantosLeao@users.noreply.github.com>
2022-06-02 11:11:07 -0300

    Update Baralho-OBI.c

--- Baralho-OBI.c
+++ Baralho-OBI.c
@@ -1,19 +1,10 @@
 #include<stdio.h>
 #include<string.h>
 
-/*int strlen(char entrada[156]){
-	int total=0;							//Função para contagem de caracteres
-	
-	while(entrada[total] != '\0'){
-		total++;
-	}
-	return total;
-} */
-
 int main(){
 	int i, j, letras;			//contadores
 	char entrada[156], comparar; //varias char de entrada e outra que compara valores 
-	int copas=0, totalcopas=13; //variaveis para as operaçoes do naipe de copas
+	int copas=0, totalcopas=13; //variaveis para as operaÃ§oes do naipe de copas
 	int espada=0, totalespada=13;	//variaveis para as operacoes do naipe de espadas
 	int ouro=0, totalouro=13;		//variaveis para as operacoes do naipe de ouro
 	int paus=0, totalpaus=13;		//variaveis para as operacoes do naipe de ouro
@@ -22,24 +13,22 @@ int main(){
 	
 	scanf("%s", entrada);
 	
-	letras= strlen(entrada);
-	printf("%d",letras);
-	//letras = strlen(entrada);		//variavel com o total de entrada de caracteres
+	letras= strlen(entrada);		//variavel com o total de entrada de caracteres
 
 	if(letras%3==0){
 		
 		for(i=1;i<=letras;i++){
 		
-			comparar = entrada[i-1];        //Adiciona o caracter na variavel para fazer a comparaçaõ dentro das estruras de condição
+			comparar = entrada[i-1];        //Adiciona o caracter na variavel para fazer a comparaÃ§aÃµ dentro das estruras de condiÃ§Ã£o
 			
-			if(i%3==1 && comparar=='0'){	//Codição para entrada quando os primeiros algarismo são 0
-				if(entrada[i+1]=='c'||entrada[i+1]=='C'){   //Condição para verificar se o "terceiro" caracter é copas
+			if(i%3==1 && comparar=='0'){	//CodiÃ§Ã£o para entrada quando os primeiros algarismo sÃ£o 0
+				if(entrada[i+1]=='c'||entrada[i+1]=='C'){   //CondiÃ§Ã£o para verificar se o "terceiro" caracter Ã© copas
 					copas+=1;
 					
-					for(j=1;j<10;j++){				//estrutura de repetição para contar os algarismo de 1 a 9
+					for(j=1;j<10;j++){				//estrutura de repetiÃ§Ã£o para contar os algarismo de 1 a 9
 						
 						if((int)entrada[i]-48==j){	//codicional que verifica o valor do "segundo" caracter entrado
-							if(restricaocopas[j]!=j){		//condicao de restrição. caso não seja verdadeiro houve a entrada de um numero repetido. Linha 43
+							if(restricaocopas[j]!=j){		//condicao de restriÃ§Ã£o. caso nÃ£o seja verdadeiro houve a entrada de um numero repetido. Linha 43
 								totalcopas-=1;
 								restricaocopas[j]=j;
 								
@@ -49,13 +38,13 @@ int main(){
 						}
 					}
 				}	
-			}else{							//Codição para entrada quando os primeiros algarismo são 1. Ctrl C+ Ctrl V na condicional de cima
+			}else{							//CodiÃ§Ã£o para entrada quando os primeiros algarismo sÃ£o 1. Ctrl C+ Ctrl V na condicional de cima
 				if(i%3==1 && comparar=='1'){
-					if(entrada[i+1]=='c'||entrada[i+1]=='C'){   //Condição para verificar se o "terceiro" caracter é copas
+					if(entrada[i+1]=='c'||entrada[i+1]=='C'){   //CondiÃ§Ã£o para verificar se o "terceiro" caracter Ã© copas
 						
 						copas+=1;
 						
-						for(j=10;j<14;j++){			//contador começa do 10 pois deve se diferenciar do J, para não ocorrer a restição
+						for(j=10;j<14;j++){			//contador comeÃ§a do 10 pois deve se diferenciar do J, para nÃ£o ocorrer a restiÃ§Ã£o
 													//se fosse igual a 1, ele iria restrigir quando digitado 01 e em seguida 11
 							if((int)entrada[i]-48==j-10){   //-10 para poder igualar a entrada do "terceiro" caractere
 								if(restricaocopas[j]!=j){
@@ -243,10 +232,6 @@ int main(){
 		printf("Entrada Invalida");
 	}
 	
-	
-	
-	
-	//printf("\n\nTotal de Copas: %d\n\nCartas: %d\n\nCaracteres: %d",copas,totalcopas,letras);
 	
 	return 0;
 }
